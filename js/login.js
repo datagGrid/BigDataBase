8b17a6766d3b8908da3f1a5b39889678f8833268
zuiqingcheng65432 <1144789589@qq.com>
2018-09-11 15:35:42 +0800

    database

--- js/login.js
+++ js/login.js
@@ -4,6 +4,7 @@ window.onload=function(){
 	let oBack=document.querySelector(".back");
 	let oClose=document.querySelector(".close");
 	let oRight=document.querySelector(".right");
+	let oSubmit=document.querySelector("#submit");
 	let oError_info=document.querySelector(".error_info");
 	let oUser=document.getElementById("user");
 	let oCenterbox=document.getElementById("center_box");
@@ -12,6 +13,21 @@ window.onload=function(){
 		oShade.style.display="block";
 		oCenterbox.style.display="block";
 	}
+	oSubmit.onclick=function(){
+		if(!oUser.value){
+			oError.style.display="block";
+			oError_info.style.display="block";
+			return false;
+		}
+		else if(!oPwd.value)
+		{
+			return false;
+		}
+		else{
+			window.open("cluster.html","_self");
+		}
+	}
+	
 	oClose.onclick=function(){
 		oShade.style.display="none";
 		oCenterbox.style.display="none";
